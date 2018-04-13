a13a514dd0a37cc6dbdfb5eabc24f178e68c3a2e
Anatoly <a.bourov@msn.com>
2018-04-13 18:23:16 +0300

    add compile to test

--- private/inbox_anatoly/test/Inbox.test.js
+++ private/inbox_anatoly/test/Inbox.test.js
@@ -2,11 +2,12 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3=new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
 
-beforeEach(()=>{
-  web3.eth.getAccounts().then(fetchedAccounts=>{
-    console.log(fetchedAccounts);
-  });
+let accounts;
+
+beforeEach(async ()=>{
+  accounts = await web3.eth.getAccounts();
 });
 
 describe('Inbox',()=>{

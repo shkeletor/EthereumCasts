81acbe89cbe82238e26eee2d117eeec1875d4141
Charles Finney <charlesfinney@gmail.com>
2018-04-16 10:27:02 -0400

    Add files via upload

--- finney/test/Inbox.test.js
+++ finney/test/Inbox.test.js
@@ -1,22 +1,39 @@
 const assert = require('assert');
-const ganach = require('ganache-cli');
+const ganache = require('ganache-cli');
 const Web3 = require('Web3');
-const web3 = new Web3(ganache.provider());
 
 
-class Car {
+// UPDATE THESE TWO LINES RIGHT HERE!!! <--------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-  park() {
-    return "stopped";
-  }
-  drive() {
-    return "vroom";
-  }
-}
 
-describe('Car', () => {
-  it('can park', () => {
-    const car = new Car();
-    assert.equal(car.park(), 'stopped');
-  });
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+// Get a list of alll accounts
+accounts = await web3.eth.getAccounts()
+
+
+// Use one of those accoutss to deploy
+// the contract
+inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data: bytecode, arguments:['Hi there!'] })
+  .send({ from: accounts[0], gas: '1000000'});
+
+  // ADD THIS ONE LINE RIGHT HERE!!! <--------
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+      assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+      const massage = await inbox.methods.message().call();
+      assert.equal(message, 'Hi there!');
+      })
 });
